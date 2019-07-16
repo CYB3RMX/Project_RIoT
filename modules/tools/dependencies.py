eba2fa066db36718765619a2f06e7144ba5c0df4
CYB3RMX <cyb3rmx0@gmail.com>
2019-07-17 01:22:31 +0300

    News 17/07/2019 01:23

--- modules/tools/dependencies.py
+++ modules/tools/dependencies.py
@@ -4,7 +4,7 @@
 
 try:
    import pycvesearch
-except ModuleNotFoundError:
+except ImportError:
    print("[!] Error: You should install >pycvesearch< module to use this tool")
    print("[+] Try: >git clone https://github.com/cve-search/PyCVESearch< then >python setup.py install<")
 import os
