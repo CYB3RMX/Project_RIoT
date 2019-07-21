99d023cc6e5ede74505bf807d2ff82ddd35ee16e
CYB3RMX <cyb3rmx0@gmail.com>
2019-07-21 20:12:30 +0300

    News 21/07/2019 20:12

--- modules/tools/dependencies.py
+++ modules/tools/dependencies.py
@@ -10,3 +10,7 @@ except ImportError:
 import os
 os.system("command -v nmap > /dev/null 2>&1 || { echo >&2 'This program needs nmap. Please install it to use this tool.' ; exit 1; }")
 os.system("command -v curl > /dev/null 2>&1 || { echo >&2 'This program needs curl. Please install it to use this tool.' ; exit 1; }")
+try:
+   import bs4
+except ImportError:
+   print("Please install bs4 module to use this program successfully...")
